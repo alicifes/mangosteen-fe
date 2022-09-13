a474e62a679ea2ff1ea8f8f9484880b9fa38bff2
alicifes <635023574@qq.com>
2022-09-13 16:05:26 +0800

    完成第一个界面

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { defineComponent } from "vue";
 import { RouterView } from "vue-router";
+import './assets/stylesheets/vars.scss'
 import "./App.scss"
 
 export const App = defineComponent({
--- src/welcome/First.tsx
+++ src/welcome/First.tsx
@@ -1,7 +1,20 @@
 import { defineComponent } from "vue";
+import s from './First.module.scss'
+import pig from '../assets/icons/pig.svg'
 
 export const First = defineComponent({
   setup() {
-    return () => <div>First</div>;
+    return () => (
+        <div class={s.wrapper}>
+          <div class={s.card}>
+            <img src={pig} />
+            <h2>会省钱<br/>还要会挣钱</h2>
+          </div>
+          <div class={s.actions}>
+              <router-link to={'/'} class={s.fake}>跳过</router-link>
+              <router-link to={'/2'}>下一页</router-link>
+              <router-link to={'/start'}>跳过</router-link>
+          </div>
+        </div>)
   },
 });
