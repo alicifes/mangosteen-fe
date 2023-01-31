e550c3b7904145527cff49d71c3a4063010a8244
alicifes <635023574@qq.com>
2023-01-31 14:25:50 +0800

    基本完成了overlay的开发

--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,18 +1,21 @@
 import { defineComponent, PropType } from "vue";
 import s from './Icon.module.scss'
 
-export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig' | 'menu'
+export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig' | 'menu' | 'export' | 'notify'
 
 export const Icon = defineComponent({
   props: {
     name: {
         type: String as PropType<IconName>,
         required:true
+    },
+    onclick:{
+      type: Function as PropType<(e:MouseEvent) => void>
     }
 },
   setup: (props, context) => {
     return () => (
-      <svg class={s.icon}>
+      <svg class={s.icon} onClick={props.onclick}>
         <use xlinkHref={"#" + props.name}></use>
       </svg>
     );
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,21 +1,26 @@
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { Button } from "../shared/Button";
 import { Center } from "../shared/Center";
 import { FloatButton } from "../shared/FloatButton";
 import { Icon } from "../shared/Icon";
 import { Navbar } from "../shared/Navbar";
+import { Overlay } from "../shared/Overlay";
 import s from "./StartPage.module.scss";
 
 export const StartPage = defineComponent((props, context) => {
+  const overlayVisible = ref(false)
   const onClick = () => {
     console.log("click");
   };
+  const onClickMenu = () => {
+    overlayVisible.value = !overlayVisible.value
+  }
   return () => (
     <>
     <Navbar>{
       {
         default: () =>'山竹记账',
-        icon: () => <Icon name='menu' class={s.navIcon}></Icon>
+        icon: () => <Icon name='menu' class={s.navIcon} onclick={onClickMenu}></Icon>
       }
     }</Navbar>
     <Center class={s.pig_wrapper}>
@@ -25,6 +30,7 @@ export const StartPage = defineComponent((props, context) => {
         <Button class={s.button} onClick={onClick}>开始记账</Button>
       </div>
       <FloatButton />
+      {overlayVisible.value && <Overlay onClose={() => overlayVisible.value = false} />}
     </>
   );
 });
