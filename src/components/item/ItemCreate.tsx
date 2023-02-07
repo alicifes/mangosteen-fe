ac7784033a592303b69a5f1175bd6f028b1897b1
alicifes <635023574@qq.com>
2023-02-07 15:13:00 +0800

    实现tab的点击效果

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,12 +1,14 @@
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, ref } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Icon } from "../../shared/Icon";
+import { Tab, Tabs } from "../../shared/Tabs";
 import s from "./ItemCreate.module.scss";
 export const ItemCreate = defineComponent({
   props: {
     name: { type: String as PropType<string> },
   },
   setup: (props, context) => {
+    const kind = ref('支出')
     return () => (
       <div>
         <MainLayout>
@@ -14,7 +16,10 @@ export const ItemCreate = defineComponent({
             title: () => "山竹记账",
             icon: () => <Icon name="back" class={s.navIcon}></Icon>,
             default: () => <div>
-                main
+              <Tabs selected={kind.value} onUpdateSelected = {(e)=> kind.value = e}>
+                <Tab name='收入'>收入</Tab>
+                <Tab name= '支出'>支出</Tab>
+              </Tabs>
             </div>,
           }}
         </MainLayout>
